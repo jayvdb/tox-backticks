6f30e0d9ee99fede09a37631aad5d1375fb5d354
John Vandenberg <jayvdb@gmail.com>
2019-07-17 10:49:17 +0700

    Process variables without dependencies first
    Fixes https://github.com/jayvdb/tox-backticks/issues/6

--- tox_backticks.py
+++ tox_backticks.py
@@ -1,3 +1,4 @@
+import itertools
 import re
 
 import pluggy
@@ -23,6 +24,10 @@ def _get_used_envvars(value):
 def _run_backtick(reader, venv, variable):
     setenv = venv.envconfig.setenv
     value = setenv.definitions[variable]
+    # non-backtick values may also pass through here
+    if not value.startswith('`') or not value.endswith('`'):
+        return
+
     cmdstr = value[1:-1]
 
     argvlist = _ArgvlistReader.getargvlist(reader, cmdstr, replace=True)
@@ -50,8 +55,12 @@ def tox_runtest_pre(venv):
     """Post process config after parsing."""
     setenv = venv.envconfig.setenv
 
+    # Invalid variable names are not our concern, but typically they
+    # have a default value defined.
+    valid_variable_names = set(setenv.definitions)
+
     backtick_variables = dict(
-        (variable, _get_used_envvars(value))
+        (variable, _get_used_envvars(value) & valid_variable_names)
         for variable, value in setenv.definitions.items()
         if len(value) > 2 and value.startswith('`') and value.endswith('`')
     )
@@ -60,20 +69,56 @@ def tox_runtest_pre(venv):
 
     reader = setenv.reader
 
-    while backtick_variables:
-        if len(backtick_variables) == 1:
-            variable = list(backtick_variables)[0]
-            _run_backtick(reader, venv, variable)
-            break
+    backtick_variables_keys = set(backtick_variables.keys())
 
-        unresolved = set(backtick_variables)
-        resolved = [
+    # Any non-backtick variable used in backticks to needs to be checked
+    used_non_backtick_variables = any(
+        uses - backtick_variables_keys for uses in backtick_variables.values()
+    )
+
+    if used_non_backtick_variables:
+        # Load `uses` of all non-backtick variables
+        non_backtick_variables = dict(
+            (variable, _get_used_envvars(value) & valid_variable_names)
+            for variable, value in setenv.definitions.items()
+            if variable not in backtick_variables
+        )
+
+        # Walk back up chain of variables that refer to a backtick variable
+        # and include them in the backtick variables
+        unresolved = True
+        while unresolved:
+            keys = set(non_backtick_variables)
+
+            unresolved = set([
+                variable for variable, uses in non_backtick_variables.items()
+                if uses - keys
+            ])
+
+            if unresolved:
+                # Any variable that refers to a backtick variable needs to be
+                # processed as a backtick variable, and other other non-backtick
+                # variables need to be re-checked
+                for variable in unresolved:
+                    backtick_variables[variable] = non_backtick_variables[variable]
+                    del non_backtick_variables[variable]
+            else:
+                # All remaining non_backtick_variables can be ignored
+                for uses in backtick_variables.values():
+                    uses -= keys
+
+    while backtick_variables:
+
+        resolved = set([
             variable for variable, uses in backtick_variables.items()
-            if not uses.intersection(unresolved)
-        ]
+            if not uses
+        ])
 
         for variable in resolved:
             _run_backtick(reader, venv, variable)
             del backtick_variables[variable]
+
+        for uses in backtick_variables.values():
+            uses -= resolved
 
     venv.envconfig.commands = reader.getargvlist('commands')
