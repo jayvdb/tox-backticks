bb16480fbc1b44764b0ddfe5ac64fef48812bb7a
John Vandenberg <jayvdb@gmail.com>
2021-02-24 09:45:43 +0800

    setup.py: Set version to 0.3.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ Development Status :: 4 - Beta
 
 setup(
     name='tox-backticks',
-    version='0.2.0',
+    version='0.3.0',
     description='Tox plugin to allow use of backticks for subcommands',
     license='MIT',
     author_email='jayvdb@gmail.com',
